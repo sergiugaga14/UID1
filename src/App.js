d8fe4b483e167edbc9c12f4ca08869c2b74b9d75
Gaga Sergiu Alexandru <Gaga.Vi.Sebastian@student.utcluj.ro>
2022-11-08 02:39:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React,{ useEffect, useState } from "react"
+import axios from 'axios'
+import JokeCard from './JokeCard';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [data,setData]=useState([]);
+  const[all,setAll]=useState(true);
+
+  const handleClick=(name)=>
+  {
+    name==="All"? setAll(true): setAll(false);
+    name==="All"?
+    axios.get( "https://v2.jokeapi.dev/joke/"+"Any"+"?amount=10")
+    .then(({data})=>{ setData(data.jokes) })
+    .catch(err=>console.log(err))
+    :axios.get( "https://v2.jokeapi.dev/joke/"+name+"?amount=10")
+    .then(({data})=>{ setData(data.jokes) })
+    .catch(err=>console.log(err))
+    
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+   
+  }
+
+  useEffect(() => {
+    axios.get( "https://sv443.net/jokeapi/v2/joke/Any?amount=10")
+    .then(({data})=>{ setData(data.jokes) })
+    .catch(err=>console.log(err));
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar bg="dark" expand="lg" variant='dark' >
+      <Container fluid>
+        <Navbar.Brand style={{fontFamily: 'Bebas Neue',fontSize:"30px"}} >JOKES</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link  href="#All" active={all} onClick={()=>handleClick("All")}>All</Nav.Link>
+            <Nav.Link href="#Christmas" onClick={()=>handleClick("Christmas")} >Christmas</Nav.Link>
+            <Nav.Link href="#Programming" onClick={()=>handleClick("Programming")}>Programming</Nav.Link>
+            <Nav.Link href="#Miscellaneous" onClick={()=>handleClick("Miscellaneous")}>Miscellaneous</Nav.Link>
+            <Nav.Link href="#Dark" onClick={()=>handleClick("Dark")}>Dark</Nav.Link>
+            <Nav.Link href="#Pun" onClick={()=>handleClick("Pun")}>Pun</Nav.Link>
+            <Nav.Link href="#Spooky" onClick={()=>handleClick("Spooky")}>Spooky</Nav.Link>
+
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      { (console.log(data),loading)? 
+        
+          <div className="loader-container">
+            <div className="spinner"></div>
+          </div>
+        :
+        <div >
+          <JokeCard jokes={data}/>
+        
+        </div>
+      }
     </div>
   );
 }
---
+++ src/JokeCard.js
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react"
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+function JokeCard(props) {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleClose2 = () => {
+        setShow(false);
+        let map = colors;
+        map.set(id, "rgba(255,130,130,0.6)");
+        setColors(map);
+    }
+    const handleShow = (id) => {
+        setShow(true);
+        setId(id);
+    }
+    const [id, setId] = useState(0);
+    const [jokes, SetJokes] = useState([]);
+    const [colors, setColors] = useState(new Map());
+
+    useEffect(() => {
+        SetJokes(props.jokes);
+        let map = new Map();
+        for (let j of props.jokes) {
+            map.set(j.id, "rgba(255,255,255,0.9)");
+        }
+        setColors(map);
+    }, [props.jokes]);
+
+
+    return (
+        <div className='d-flex flex-wrap justify-content-center joke'>
+            {jokes && jokes.map((item) => (
+                <div className='p-2 flex-fill ' key={item.id}  >
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                    <div id="container" style={{ background: colors.get(item.id) }}>
+
+                        <div className="product-details" >
+
+                            <h1>{item.category}</h1>
+                            <br></br>
+                            <h1>{item.type}</h1>
+                            <br></br>
+                            <br></br>
+                            {
+                                item.type === "single" ?
+                                    <p style={{ fontSize: "20px" }}>{item.joke}</p>
+                                    : <p >{item.setup}</p>
+                            }
+
+                            <div className='d-flex  justify-content-start'>
+                                {
+                                    Object.entries(item.flags).map((fl) => (<div key={fl[0]}>
+                                        {fl[1] && <div className='p-2'> <Badge bg="dark" size="lg">{fl[0]}</Badge></div>}
+                                    </div>
+                                    ))
+                                }
+                            </div>
+                            <div className="control">
+
+                                <Button variant="dark" size="lg" onClick={() => handleShow(item.id)}>Report</Button>
+
+                            </div>
+
+                        </div>
+                        {
+                            item.type !== "single" &&
+                            <div className="product-image">
+                                <div className="info">
+                                    <h2> Continuation</h2>
+                                    {item?.delivery}
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
+            ))}
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Report joke</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to report the joke?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="dark" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="danger" onClick={handleClose2}>
+                        Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+export default JokeCard;
